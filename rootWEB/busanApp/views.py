f56841b82cd32deb898e57e03d1c22c482ffbb07
jinuwuu <jin82232@naver.com>
2023-12-08 17:13:04 +0900

    jinuwuu commit2

--- rootWEB/busanApp/views.py
+++ rootWEB/busanApp/views.py
@@ -4,7 +4,7 @@ from django.db import connections
 # Create your views here.
 def fetch_population_info_view(request):
     with connections['default'].cursor() as cursor:
-        cursor.execute("SELECT * FROM reigon_type_count")
-        population_info = cursor.fetchall()
+         cursor.execute("SELECT * FROM reigon_type_count")
+         population_info = cursor.fetchall()
 
         return render(request, 'busan/index.html', {'population_info': population_info})
