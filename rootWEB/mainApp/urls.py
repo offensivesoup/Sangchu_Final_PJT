95f5da04955072898a7f1c4cf3804388dbb5a174
hun <qwh0765@gmail.com>
2023-12-08 16:39:43 +0900

    map commit

--- rootWEB/mainApp/urls.py
+++ rootWEB/mainApp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 from mainApp import views
 urlpatterns = [
     path("", views.index),
+    path("map/", views.map),
+
 ]
--- rootWEB/mainApp/views.py
+++ rootWEB/mainApp/views.py
@@ -3,4 +3,7 @@ def index(request) :
     print('debug >>> client path, mainApp/index, render = index')
     return render(request, 'main/index.html')
 
+def map(request) :
+    return render(request, 'main/map.html')
+
 # Create your views here.
