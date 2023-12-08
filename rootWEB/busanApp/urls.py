11163b200786a51fcc30437589b25f32d71d5d70
jinuwuu <jin82232@naver.com>
2023-12-08 17:11:43 +0900

    jinuwuu commit

--- rootWEB/busanApp/urls.py
+++ rootWEB/busanApp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from busanApp import views
+from . import views
+from .views import fetch_population_info_view
+
 urlpatterns = [
-    path("", views.index),
+    path("", views.fetch_population_info_view),
 ]
--- rootWEB/busanApp/views.py
+++ rootWEB/busanApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.db import connections
 
 # Create your views here.
-def index(request):
-    pass
+def fetch_population_info_view(request):
+    with connections['default'].cursor() as cursor:
+        cursor.execute("SELECT * FROM reigon_type_count")
+        population_info = cursor.fetchall()
+
+        return render(request, 'busan/index.html', {'population_info': population_info})
