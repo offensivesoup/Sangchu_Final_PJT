f836908a0e7f3a86d5b212db60067799c7015c90
jszxro <jszxro@gmail.com>
2023-12-08 17:12:42 +0900

    jsxzro commit

--- rootWEB/busanApp/urls.py
+++ rootWEB/busanApp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
-from busanApp import views
+# from busanApp import views
+from . import views
+from .views import fetch_lease_trend_view, fetch_zipgac_numbers_view
 urlpatterns = [
-    path("busan/", views.index, name='busan'),
+    path("", views.fetch_lease_trend_view, name='busan_lease_trend'),
+    path("", views.fetch_zipgac_numbers_view, name='busan_zipgac_numbers'),
 ]
--- rootWEB/busanApp/views.py
+++ rootWEB/busanApp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    pass
+from django.db import connections
+
+def fetch_lease_trend_view(request):
+    with connections['default'].cursor() as cursor:
+        cursor.execute("SELECT * FROM lease_trend")
+        lease_trend = cursor.fetchall()
+
+    return render(request, 'busan/index.html', {'lease_trend': lease_trend})
+
+def fetch_zipgac_numbers_view(request):
+    with connections['default'].cursor() as cursor:
+        cursor.execute("SELECT * FROM zipgac_numbers")
+        zipgac_numbers = cursor.fetchall()
+
+    return render(request, 'busan/index.html', {'zipgac_numbers': zipgac_numbers})
