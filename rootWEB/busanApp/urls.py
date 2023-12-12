666bfd952781d4f3ba3608155ae38a717847168a
blueberryade <blueberryade7@gmail.com>
2023-12-12 15:24:25 +0900

    feat: 지도 추가

--- rootWEB/busanApp/urls.py
+++ rootWEB/busanApp/urls.py
@@ -1,8 +1,10 @@
 from os import path
 from django.urls import path, include
 from . import views
+from busanApp import views
 from .views import json_store_density_view,json_lease_trend_view,json_zipgac_number_view,json_population_density_view, json_service_density_view, json_population_cnt_view, predict_model_view
 urlpatterns = [
+    path("", views.index),
     path("json1/", json_store_density_view    , name='json_store_density_view'),
     path("json2/", json_lease_trend_view      , name='json_lease_trend_view'),
     path("json3/", json_zipgac_number_view    , name='json_lease_trend_view'),
