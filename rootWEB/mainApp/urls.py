c9263690c103c6009b21fde4b188b62c35075c2f
blueberryade <blueberryade7@gmail.com>
2023-12-13 16:58:06 +0900

    conflict 해결

--- rootWEB/mainApp/urls.py
+++ rootWEB/mainApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("map/", views.map),
     path("signup/",views.signup),
     path("busan/",views.busan),
-    path("analysis1/",views.analysis_lease)
+    path("analysis1/",views.analysis_lease),
     path("sign2/",views.sign2),
 
 
