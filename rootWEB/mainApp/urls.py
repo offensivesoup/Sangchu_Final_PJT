95f427100de89cd99887c9c85256435d480563e0
jinuwuu <jin82232@naver.com>
2023-12-14 13:57:11 +0900

    add gitignore

--- rootWEB/mainApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from mainApp import views
-urlpatterns = [
-    path("", views.index),
-    path("map/", views.map),
-    path("busan/",views.busan),
-    path("sign2/",views.sign2),
-    path("analysis1/",views.analysis_lease)
-]
