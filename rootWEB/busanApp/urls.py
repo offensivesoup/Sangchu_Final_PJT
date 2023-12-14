f2fe380352853508fcc30558f3d21855d99c659a
dhwan <ehdghks9719@naver.com>
2023-12-14 14:52:39 +0900

    dhwan commit

--- rootWEB/busanApp/urls.py
+++ rootWEB/busanApp/urls.py
@@ -2,11 +2,8 @@ from os import path
 from django.urls import path, include
 from . import views
 from .views import json_store_density_view,json_lease_trend_view,json_zipgac_number_view,json_population_density_view, json_service_density_view, json_population_cnt_view, predict_model_view
-<<<<<<< HEAD
-from .views import cosine_similarity_view, population_density_chart, get_lease_trend_data, lease_trend, service_population_json, service_population
-=======
-from .views import cosine_similarity_view
->>>>>>> jinuwuu1214
+
+from .views import cosine_similarity_view, get_lease_trend_data, lease_trend, service_population_json, service_population
 urlpatterns = [
     path("", views.index),
     path("json1/", json_store_density_view             , name='json_store_density_view'),
@@ -18,11 +15,10 @@ urlpatterns = [
     path('predict/', predict_model_view                , name='predict_model_view'),
     path('cosine/<int:index>/', cosine_similarity_view , name = 'cosine'),
     path('json_population_density_view/'     , json_population_density_view, name='json_population_density_view'),
-    path('population_density_chart/'         , population_density_chart, name='population_density_chart'),
     path('get_lease_trend_data/'             , get_lease_trend_data, name='get_lease_trend_data'),
     path('lease_trend/'                      , lease_trend, name='lease_trend'),
     path('service_population_json/'          , service_population_json, name='service_population_json'),
     path('service_population/'               , service_population, name='service_population'),
     path('json_population_density_view_all/', views.json_population_density_view_all, name='json_population_density_view_all'),
-    path('json_population_cnt_view/', views.json_population_cnt_view, name ='json_population_cnt_view')
+    path('json_population_cnt_view/', views.json_population_cnt_view, name ='json_population_cnt_view')]
 
