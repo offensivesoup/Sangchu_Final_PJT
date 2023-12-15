b145c0b555afacfe44d0e728ac2e69a036d2885e
hun <qwh0765@gmail.com>
2023-12-15 13:24:57 +0900

    density chart commit

--- rootWEB/busanApp/urls.py
+++ rootWEB/busanApp/urls.py
@@ -2,8 +2,7 @@ from os import path
 from django.urls import path, include
 from . import views
 from .views import json_store_density_view,json_lease_trend_view,json_zipgac_number_view,json_population_density_view, json_service_density_view, json_population_cnt_view, predict_model_view
-
-from .views import cosine_similarity_view, get_lease_trend_data, lease_trend, service_population_json, service_population
+from .views import cosine_similarity_view, service_population_json, service_population, store_density_json, store_density
 urlpatterns = [
     path("", views.index),
     path("json1/", json_store_density_view             , name='json_store_density_view'),
@@ -15,10 +14,12 @@ urlpatterns = [
     path('predict/', predict_model_view                , name='predict_model_view'),
     path('cosine/<int:index>/', cosine_similarity_view , name = 'cosine'),
     path('json_population_density_view/'     , json_population_density_view, name='json_population_density_view'),
-    path('get_lease_trend_data/'             , get_lease_trend_data, name='get_lease_trend_data'),
-    path('lease_trend/'                      , lease_trend, name='lease_trend'),
+    # path('get_lease_trend_data/'             , get_lease_trend_data, name='get_lease_trend_data'),
+    # path('lease_trend/'                      , lease_trend, name='lease_trend'),
     path('service_population_json/'          , service_population_json, name='service_population_json'),
     path('service_population/'               , service_population, name='service_population'),
     path('json_population_density_view_all/', views.json_population_density_view_all, name='json_population_density_view_all'),
-    path('json_population_cnt_view/', views.json_population_cnt_view, name ='json_population_cnt_view')]
+    path('json_population_cnt_view/', views.json_population_cnt_view, name ='json_population_cnt_view'),
+    path('store_density_json/', store_density_json, name = 'store_density_json'),
+    path('store_density/', store_density, name = 'store_density')]
 
