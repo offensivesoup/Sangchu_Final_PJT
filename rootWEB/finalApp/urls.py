99ec5ef5358b3dce725df8e43b1b76c72852d229
blueberryade <blueberryade7@gmail.com>
2023-12-18 11:15:19 +0900

    Add: 공실 상세페이지

--- rootWEB/finalApp/urls.py
+++ rootWEB/finalApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from finalApp import views
 urlpatterns = [
     path("",views.list),
+    path("detail/<int:maemul_id>",views.detail)
 
 ]
--- rootWEB/finalApp/views.py
+++ rootWEB/finalApp/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def list(request):
     print('debug >>> client path, finalApp/list, render = list')
     return render(request,'final/list.html')
+
+def detail(request,maemul_id):
+    print('debug >>> maemul_id: ',maemul_id)
+    print('debug >>> client path, finalApp/detail, render = detail')
+    return render(request, 'final/detail.html')
