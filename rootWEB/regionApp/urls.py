c96078d6b0e38fe7c11c7054e4279ee76e3562ec
hun <qwh0765@gmail.com>
2023-12-21 17:31:35 +0900

    map visualized update

--- rootWEB/regionApp/urls.py
+++ rootWEB/regionApp/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("<str:region_name>/chart4/", get_final_moving_data, name = 'get_final_moving_data'),
     path("<str:region_name>/chart5/",get_rent_mean_data, name = 'get_rent_mean_data'),
     path("<str:region_name>/chart6/",get_subway_session_data, name = 'get_subway_session_data'),
-    path("<str:region_name>/chart7/",get_eval_score_data, name = 'get_eval_score_data')]
+    path("<str:region_name>/chart7/",get_eval_score_data, name = 'get_eval_score_data'),
+    path("<str:region_name>/school_json/", school_json, name = 'school_json'),
+    path("<str:region_name>/subwayloc_json/", subwayloc_json, name='subwayloc_json')]
