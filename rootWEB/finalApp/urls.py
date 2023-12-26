7a64a53ffa7cb6f7d4f4f77504c0919c88fd147a
blueberryade <blueberryade7@gmail.com>
2023-12-26 14:51:46 +0900

    fix:like

--- rootWEB/finalApp/urls.py
+++ rootWEB/finalApp/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("<str:region_name>/<int:maemul_id>/detail/getData/",views.get_detail),
     path('<str:region_name>/<int:maemul_id>/detail/cosine/', cosine_similarity_view , name = 'cosine'),
     path('<str:region_name>/<int:maemul_id>/detail/view/', detail_view, name = "views"),
-    path("like_view/",views.like_view, name = "like_view")
+    path("like_view/<int:maemul_id>/<int:user_id>",views.like_view, name = "like_view"),
+    path('<str:region_name>/<int:maemul_id>/detail/like_status/',views.like_status)
 ]
