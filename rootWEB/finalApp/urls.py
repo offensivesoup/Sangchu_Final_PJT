25966725524a2fe41feb0b93026dbbaeaa167f01
dhwan <ehdghks9719@naver.com>
2023-12-27 15:47:10 +0900

    solve conflict

--- rootWEB/finalApp/urls.py
+++ rootWEB/finalApp/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('<str:region_name>/<int:maemul_id>/detail/view/', detail_view, name = "views"),
     path("like_view/",views.like_view, name = "like_view"),
     path("like_view/<int:maemul_id>/<int:user_id>",views.like_view, name = "like_view"),
-    path('<str:region_name>/<int:maemul_id>/detail/like_status/',views.like_status)
+    # path('<str:region_name>/<int:maemul_id>/detail/like_status/',views.like_status)
 ]
