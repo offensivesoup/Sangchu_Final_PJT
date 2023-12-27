09b8a93d27ea71ac9b88e80043cbc16d83ad615f
hun <qwh0765@gmail.com>
2023-12-27 13:03:48 +0900

    map temp v1

--- rootWEB/finalApp/urls.py
+++ rootWEB/finalApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from finalApp import views
-from finalApp.views import cosine_similarity_view, detail_view
+from finalApp.views import cosine_similarity_view, detail_view, gongsilmap
 
 urlpatterns = [
     path("<str:region_name>/",views.list),
@@ -8,5 +8,6 @@ urlpatterns = [
     path("<str:region_name>/getData/",views.get_list),
     path("<str:region_name>/<int:maemul_id>/detail/getData/",views.get_detail),
     path('<str:region_name>/<int:maemul_id>/detail/cosine/', cosine_similarity_view , name = 'cosine'),
-    path('<str:region_name>/<int:maemul_id>/detail/view/', detail_view, name = "views")
+    path('<str:region_name>/<int:maemul_id>/detail/view/', detail_view, name = "views"),
+    path('<str:region_name>/', views.gongsilmap)
 ]
